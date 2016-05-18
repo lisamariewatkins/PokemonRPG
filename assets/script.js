1e6bd433f0f10a4fe39f5564d0dbc74fc907a327
Lisa Watkins <Lisa Watkins>
2016-05-18 14:01:57 -0500

    updated player movements

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,124 @@
+var characterHTML = "";
+
+var hero;
+
+var opponents;
+
+var defender;
+
+var villian = "";
+
+var playerId;
+
+var defenderId;
+
+var pickedCharacter = false;
+
+var pickedDefender = false;
+
 $(document).ready(function(){
 
-	var myHTML = "";
+	
 
-	var playerArray = [{
-	name: "charmandar",
-	img: "assets/img/charmandar.png",
-	healthPoints: 10,
-	attackPoints: 4,
-	counterAttackPoints: 4},
+	function startGame(){
+		for (var i=0; i<playerArray.length; i++){
+			characterHTML = "<div class='characterBin'><img class=characterImage src=" + playerArray[i].img + "><p>" + playerArray[i].name + "</p><p class='health'>Health Points:" + playerArray[i].healthPoints + "</p></div>";
+			$("#playerList").append(characterHTML);
+		};
+		$("#yourPokemonIs").hide();
+		$("#selectFirstOpponent").hide(); 
+		$(".btn").hide();
+	};
 
-	{name: "pikachu",
-	img: "assets/img/pikachu.png",
-	healthPoints: 10,
-	attackPoints: 8,
-	counterAttackPoints: 8},
+	function attack(){
+		
+	};
 
-	{name: "meowth",
-	img: "assets/img/meowth.png",
-	healthPoints: 10,
-	attackPoints: 16,
-	counterAttackPoints: 16},
+	function counterAttack(){
+		//computer attack code here
+	};
 
-	{name: "squirtle",
-	img: "assets/img/squirtle.png",
-	healthPoints: 10,
-	attackPoints: 2,
-	counterAttackPoints: 2}]
+	var playerArray = [
+	{
+		name: "charmander",
+		img: "assets/img/charmander.png",
+		healthPoints: 10,
+		attackPoints: 4,
+		counterAttackPoints: 4
+	},
 
-	for (var i = 0; i < playerArray.length; i++){
-		myHTML += "<div id=" + i + "><p>" + playerArray[i].name + "</p><div class='inner-img-container'><img class='characterImage' src=" + heroArray[i].img + "></div><p class='health'</p>" + "health: " + playerArray[i].healthPoints + "</div>"
-	}
+	{
+		name: "pikachu",
+		img: "assets/img/pikachu.png",
+		healthPoints: 10,
+		attackPoints: 8,
+		counterAttackPoints: 8
+	},
 
-	$("#selectPlayer").html(myHTML);
+	{
+		name: "meowth",
+		img: "assets/img/meowth.png",
+		healthPoints: 10,
+		attackPoints: 16,
+		counterAttackPoints: 16
+	},
 
+	{
+		name: "squirtle",
+		img: "assets/img/squirtle.png",
+		healthPoints: 10,
+		attackPoints: 2,
+		counterAttackPoints: 2
+	}];
 
-	$("characterImage").on('click', function(){
-		var opponents = $(this).siblings;
-		$("#enemies").append(opponents);
+	startGame();
+
+	$(".characterBin").on('click', function(){
+		if (pickedCharacter == false){
+			//hides "choose" paragraph
+			$("#choose").hide();
+			//sets var hero = to clicked character
+			hero = $(this).detach();
+			opponents = $("#playerList .characterBin");
+			opponents.addClass("opponents");
+			enemyClick();
+			$("#yourHero").append(hero);
+			$("#yourEnemies").append(opponents);
+			$("#yourPokemonIs").show();
+			$("#selectFirstOpponent").show();
+			}
+		pickedCharacter = true;
 	});
+
+	function enemyClick(){
+		$(".opponents").on('click', function(){
+			if (!pickedDefender){
+				defenderId = $(this).attr("Id");
+				attackButtonClick();
+				defender = $(this).detach();
+				$("#selectFirstOpponent").hide();
+				$("#yourPokemonIs").hide();
+				$("#currentDefender").append(defender); //not appending to this div
+				$(".btn").show(); //consider visibility hidden
+			}
+			pickedDefender = true;
+		});
+	};
+
+	function attackButtonClick(){
+		$("#attackButton").on('click', function(){
+			if (pickedDefender == true){
+				playerArray[playerId].healthPoints = playerArray[playerId].attackPoints - playerArray[defenderId].counterAttackPoints;
+				playerArray[defenderId].healthPoints = playerArray[defenderId].attackPoints - playerArray[playerId].counterAttackPoints;
+				$("#yourHero .health").html("Health: " + playerArray[playerId].healthPoints);
+				$("#currentDefender .health").html("Health: " + playerArray[defenderId].healthPoints);
+			}
+		});
+	};
+
+
+	// if healthPoints == 0{
+	// 	you lost;
+	// };
 
 });
